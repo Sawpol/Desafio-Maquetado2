918de932b81e3bd0a179b097a19f69012685b161
Juan de Dios <juand10193@hotmail.com>
2021-01-19 17:08:21 -0600

    ADD FECTH GENERIC FUNCTION AND EDIT POST JS

--- js/base.js
+++ js/base.js
@@ -53,3 +53,25 @@ const showResponse = (response) => {
     showResponse,
     'DELETE'
 ) */
+
+const fetchData = (url,metod,data={})=>{
+    return new Promise((resolve, reject) => {
+        const params ={
+            method:metod,
+            body:JSON.stringify(data),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }
+    
+        fetch(url,params).then(res=> res.json())
+        .catch(error => {
+            console.error('Error:', error)
+            reject(error)
+        })
+        .then(response => {
+            console.log('Success:', response)
+            resolve(response)
+        });
+    });
+}
