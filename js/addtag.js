534ed05cd6c746f442b4c7bca49dca1349071a68
André Vázquez <sawpol123@gmail.com>
2021-01-19 19:17:06 -0600

    tag

--- js/addtag.js
+++ js/addtag.js
@@ -1,38 +1,45 @@
 console.log('<---- ... Cosole Ready TAG ---->')
 
-const urlDB = 'https://reto-js-default-rtdb.firebaseio.com'
+//const urlDB = 'https://reto-js-default-rtdb.firebaseio.com'
+//
+//// function request all methods
+//const requestAjax = (url, callback, method = 'GET', data = {}) => {
+//    let xhttp = new XMLHttpRequest();
+//    xhttp.onreadystatechange = function () {
+//        if (this.readyState === 4 && this.status === 200) {
+//            /* console.log(this.response) */
+//            callback(JSON.parse(this.response))
+//            return true
+//        } else if (this.readyState === 4 && this.status === 404) {
+//            console.error(JSON.parse(this.response))
+//            return false
+//        }
+//    }
+//    xhttp.open(method, url)
+//    if (method === 'GET' || method === 'DELETE') {
+//        xhttp.send()
+//    } else {
+//        // put, patch, post
+//        xhttp.send(JSON.stringify(data))
+//    }
+//}
+//
+//// put y post
+//const showResponse = (response) => {
+//    console.log(response)
+//}
+//
+///* requestAjax(
+//    `https://reto-js-default-rtdb.firebaseio.com/tags/.json`,
+//    showResponse,
+//    'POST', {
+//        tag: "JavaScript"
+//    }
+//) */
+//
+//
+//
+//
 
-// function request all methods
-const requestAjax = (url, callback, method = 'GET', data = {}) => {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            /* console.log(this.response) */
-            callback(JSON.parse(this.response))
-            return true
-        } else if (this.readyState === 4 && this.status === 404) {
-            console.error(JSON.parse(this.response))
-            return false
-        }
-    }
-    xhttp.open(method, url)
-    if (method === 'GET' || method === 'DELETE') {
-        xhttp.send()
-    } else {
-        // put, patch, post
-        xhttp.send(JSON.stringify(data))
-    }
-}
 
-// put y post
-const showResponse = (response) => {
-    console.log(response)
-}
-
-/* requestAjax(
-    `https://reto-js-default-rtdb.firebaseio.com/tags/.json`,
-    showResponse,
-    'POST', {
-        tag: "JavaScript"
-    }
-) */
+console.log('funcionando')
